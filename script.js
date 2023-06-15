d559549fed736f1d2e97fe80102ca001b8a0b5db
yani pranay <33549205+yanipanay@users.noreply.github.com>
2023-06-15 18:38:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ const person = {
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 var team=players;
 var team1=players.slice();
+
+let cap1 = {
+    ...person
+};
+
 window.players = players;
 window.person = person;
 window.team = team;
